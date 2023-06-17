7bcd4e6b4f6f09af97b114d8334be820af2e3915
sajjad <samaralishaikh212@gmail.com>
2023-06-17 15:48:35 +0530

    fix: typo

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -123,6 +123,7 @@ export default async function IndexPage() {
             <CardDescription>You made 265 sales this month.</CardDescription>
           </CardHeader>
           <CardContent>
+            {/* @ts-expect-error server Component */}
             <RecentSales />
           </CardContent>
         </Card>
