0cb98b1ad97d6947db4f9afd1fc13a68ed0c8d38
sajjad <samaralishaikh212@gmail.com>
2023-06-28 15:55:58 +0530

    update: overview component

--- src/components/overview.tsx
+++ src/components/overview.tsx
@@ -2,56 +2,56 @@
 
 import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts"
 
-const data = [
-  {
-    name: "Jan",
-    total: Math.floor(Math.random() * 10000) + 1000,
-  },
-  {
-    name: "Feb",
-    total: Math.floor(Math.random() * 10000) + 1000,
-  },
-  {
-    name: "Mar",
-    total: Math.floor(Math.random() * 10000) + 1000,
-  },
-  {
-    name: "Apr",
-    total: Math.floor(Math.random() * 10000) + 1000,
-  },
-  {
-    name: "May",
-    total: Math.floor(Math.random() * 10000) + 1000,
-  },
-  {
-    name: "Jun",
-    total: Math.floor(Math.random() * 10000) + 1000,
-  },
-  {
-    name: "Jul",
-    total: Math.floor(Math.random() * 10000) + 1000,
-  },
-  {
-    name: "Aug",
-    total: Math.floor(Math.random() * 10000) + 1000,
-  },
-  {
-    name: "Sep",
-    total: Math.floor(Math.random() * 10000) + 1000,
-  },
-  {
-    name: "Oct",
-    total: Math.floor(Math.random() * 10000) + 1000,
-  },
-  {
-    name: "Nov",
-    total: Math.floor(Math.random() * 10000) + 1000,
-  },
-  {
-    name: "Dec",
-    total: Math.floor(Math.random() * 10000) + 1000,
-  },
-]
+// const data = [
+//   {
+//     name: "Jan",
+//     total: Math.floor(Math.random() * 10000) + 1000,
+//   },
+//   {
+//     name: "Feb",
+//     total: Math.floor(Math.random() * 10000) + 1000,
+//   },
+//   {
+//     name: "Mar",
+//     total: Math.floor(Math.random() * 10000) + 1000,
+//   },
+//   {
+//     name: "Apr",
+//     total: Math.floor(Math.random() * 10000) + 1000,
+//   },
+//   {
+//     name: "May",
+//     total: Math.floor(Math.random() * 10000) + 1000,
+//   },
+//   {
+//     name: "Jun",
+//     total: Math.floor(Math.random() * 10000) + 1000,
+//   },
+//   {
+//     name: "Jul",
+//     total: Math.floor(Math.random() * 10000) + 1000,
+//   },
+//   {
+//     name: "Aug",
+//     total: Math.floor(Math.random() * 10000) + 1000,
+//   },
+//   {
+//     name: "Sep",
+//     total: Math.floor(Math.random() * 10000) + 1000,
+//   },
+//   {
+//     name: "Oct",
+//     total: Math.floor(Math.random() * 10000) + 1000,
+//   },
+//   {
+//     name: "Nov",
+//     total: Math.floor(Math.random() * 10000) + 1000,
+//   },
+//   {
+//     name: "Dec",
+//     total: Math.floor(Math.random() * 10000) + 1000,
+//   },
+// ]
 
 interface SalesData {
   name: string
@@ -63,11 +63,9 @@ interface OverviewProps {
 }
 
 export function Overview({ salesData }: OverviewProps) {
-  // const data = salesData.data
-  console.log(salesData)
   return (
     <ResponsiveContainer width="100%" height={350}>
-      <BarChart data={data}>
+      <BarChart data={salesData}>
         <XAxis
           dataKey="name"
           stroke="#888888"
