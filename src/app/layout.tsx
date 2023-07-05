603397a5eb2d38f55693b6b358b0eac4298c3290
sajjad <samaralishaikh212@gmail.com>
2023-07-05 13:45:27 +0530

    fix: server component error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,6 +69,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         >
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className="relative flex min-h-screen flex-col">
+              {/* @ts-expect-error server Component */}
               <SiteHeader />
               <div className="flex-1">{children}</div>
             </div>
