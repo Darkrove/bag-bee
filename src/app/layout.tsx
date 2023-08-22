e1a87a0d846d863b8298711e294cca1a59a670ba
sajjad <samaralishaikh212@gmail.com>
2023-08-22 19:44:49 +0530

    add: auth provider to fix session issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,10 @@ import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
 import { Toaster } from "@/components/ui/toaster"
+import AuthProvider from "@/components/context/auth-provider"
+import { ThemeProvider } from "@/components/context/theme-provider"
 import SiteHeader from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
-import { ThemeProvider } from "@/components/theme-provider"
 
 // export const metadata: Metadata = {
 //   title: {
@@ -68,12 +69,14 @@ export default function RootLayout({ children }: RootLayoutProps) {
           )}
         >
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <div className="relative flex min-h-screen flex-col">
-              {/* @ts-expect-error server Component */}
-              <SiteHeader />
-              <div className="flex-1">{children}</div>
-            </div>
-            <TailwindIndicator />
+            <AuthProvider>
+              <div className="relative flex min-h-screen flex-col">
+                {/* @ts-expect-error server Component */}
+                <SiteHeader />
+                <div className="flex-1">{children}</div>
+              </div>
+              <TailwindIndicator />
+            </AuthProvider>
           </ThemeProvider>
           <Toaster />
         </body>
