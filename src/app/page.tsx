94eaace1bdd0860ef9a908810af1c351b4062233
sajjad <samaralishaikh212@gmail.com>
2023-08-26 17:28:19 +0530

    update: home page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function IndexPage() {
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
         <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
           <a
-            className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
+            className="rounded-2xl bg-primary/75 px-4 py-1.5 text-sm font-medium"
             target="_blank"
             href="https://bag-bee.vercel.app/"
             rel="noreferrer"
