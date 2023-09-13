4aa2e8088342fb41f0851c67473cc2cd9da11d16
sajjad shaikh <53792139+Darkrove@users.noreply.github.com>
2023-09-13 19:19:41 +0530

    Update site-header.tsx

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -22,7 +22,7 @@ const SiteHeader = async () => {
     .join("")
     .toUpperCase()
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background/50 backdrop-blur">
+    <header className="sticky top-0 z-30 w-full border-b bg-background/50 backdrop-blur">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
