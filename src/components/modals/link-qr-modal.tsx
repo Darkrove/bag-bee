aa04f849f1ac629b743d1ca522ef8f9935558538
sajjad <samaralishaikh212@gmail.com>
2023-09-13 18:24:02 +0530

    add: leaflet for smAll screen

--- src/components/modals/link-qr-modal.tsx
+++ src/components/modals/link-qr-modal.tsx
@@ -12,6 +12,7 @@ import { Clipboard, Download, Pencil, QrCode, Share, Trash } from "lucide-react"
 
 import { QRCodeSVG, getQRAsCanvas, getQRAsSVGDataUri } from "@/lib/qr"
 import { linkConstructor } from "@/lib/utils"
+import useWindowSize from "@/hooks/useWindowSize"
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
@@ -22,8 +23,8 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { Leaflet } from "@/components/ui/leaflet"
 
 export interface SimpleLinkProps {
   domain?: string
@@ -34,6 +35,8 @@ export interface SimpleLinkProps {
 export function LinkQRModalHelper({ props }: { props: SimpleLinkProps }) {
   const [showLogo, setShowLogo] = useState(true)
   const [fgColor, setFgColor] = useState("#000000")
+  const [openPopover, setOpenPopover] = useState(false)
+  const { isMobile, isDesktop } = useWindowSize()
   const anchorRef = useRef<HTMLAnchorElement>(null)
   const qrLogoUrl = "/logo.svg"
   const qrData = useMemo(
@@ -64,67 +67,135 @@ export function LinkQRModalHelper({ props }: { props: SimpleLinkProps }) {
     anchorRef.current.click()
   }
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <button className="group rounded-full bg-gray-100 p-1.5 transition-all duration-75 hover:scale-105 hover:bg-blue-100 focus:outline-none active:scale-95">
+    <div>
+      {isMobile && (
+        <button
+          onClick={() => setOpenPopover((prev) => !prev)}
+          className="group rounded-full bg-gray-100 p-1.5 transition-all duration-75 hover:scale-105 hover:bg-blue-100 focus:outline-none active:scale-95"
+        >
           <span className="sr-only">Copy</span>
           <QrCode className="h-4 w-4 text-gray-700 transition-all group-hover:text-blue-800" />
         </button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Download QR Code</DialogTitle>
-        </DialogHeader>
-        <div className="flex flex-col space-y-6 bg-gray-50 py-6 text-left sm:rounded-b-2xl">
-          <div className="mx-auto rounded-lg border-2 border-gray-200 bg-white p-4">
-            <QRCodeSVG
-              value={qrData.value}
-              size={qrData.size / 8}
-              bgColor={qrData.bgColor}
-              fgColor={qrData.fgColor}
-              level={qrData.level}
-              includeMargin={false}
-              // @ts-ignore
-              imageSettings={
-                showLogo && {
-                  ...qrData.imageSettings,
-                  height: qrData.imageSettings
-                    ? qrData.imageSettings.height / 8
-                    : 0,
-                  width: qrData.imageSettings
-                    ? qrData.imageSettings.width / 8
-                    : 0,
-                }
-              }
-            />
+      )}
+      {openPopover && isMobile && (
+        <Leaflet setShow={setOpenPopover}>
+          <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 p-4 sm:px-16">
+            <h3 className="text-lg font-medium">Download QR Code</h3>
           </div>
-        </div>
-        <DialogFooter>
-          <div className="grid grid-cols-2 gap-2 px-4 sm:px-16">
-            <Button
-              onClick={() => navigator.clipboard.writeText(props.url)}
-              className="text-sm"
-            >
-              <Clipboard className="mr-2 h-4 w-4" /> Copy
-            </Button>
-            <Button
-              onClick={async () => {
-                const canvas = await getQRAsCanvas(qrData, "image/png")
 
-                if (canvas instanceof HTMLCanvasElement) {
-                  const canvasDataURL = canvas.toDataURL("image/png")
-                  download(canvasDataURL, "png")
-                } else {
-                  download(canvas, "png")
+          <div className="flex flex-col space-y-6 bg-gray-50 py-6 text-left sm:rounded-b-2xl">
+            <div className="mx-auto rounded-lg border-2 border-gray-200 bg-white p-4">
+              <QRCodeSVG
+                value={qrData.value}
+                size={qrData.size / 8}
+                bgColor={qrData.bgColor}
+                fgColor={qrData.fgColor}
+                level={qrData.level}
+                includeMargin={false}
+                // @ts-ignore
+                imageSettings={
+                  showLogo && {
+                    ...qrData.imageSettings,
+                    height: qrData.imageSettings
+                      ? qrData.imageSettings.height / 8
+                      : 0,
+                    width: qrData.imageSettings
+                      ? qrData.imageSettings.width / 8
+                      : 0,
+                  }
                 }
-              }}
-              className="text-sm "
-            >
-              <Download className="mr-2 h-4 w-4" /> Download
-            </Button>
+              />
+            </div>
+            <div className="grid grid-cols-2 gap-2 px-4 sm:px-16">
+              <Button
+                onClick={() => navigator.clipboard.writeText(props.url)}
+                className="text-sm"
+              >
+                <Clipboard className="mr-2 h-4 w-4" /> Copy
+              </Button>
+              <Button
+                onClick={async () => {
+                  const canvas = await getQRAsCanvas(qrData, "image/png")
+
+                  if (canvas instanceof HTMLCanvasElement) {
+                    const canvasDataURL = canvas.toDataURL("image/png")
+                    download(canvasDataURL, "png")
+                  } else {
+                    download(canvas, "png")
+                  }
+                }}
+                className="text-sm "
+              >
+                <Download className="mr-2 h-4 w-4" /> Download
+              </Button>
+            </div>
           </div>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+        </Leaflet>
+      )}
+      {isDesktop && (
+        <Dialog>
+          <DialogTrigger asChild>
+            <button className="group rounded-full bg-gray-100 p-1.5 transition-all duration-75 hover:scale-105 hover:bg-blue-100 focus:outline-none active:scale-95">
+              <span className="sr-only">Copy</span>
+              <QrCode className="h-4 w-4 text-gray-700 transition-all group-hover:text-blue-800" />
+            </button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Download QR Code</DialogTitle>
+            </DialogHeader>
+            <div className="flex flex-col space-y-6 bg-gray-50 py-6 text-left sm:rounded-b-2xl">
+              <div className="mx-auto rounded-lg border-2 border-gray-200 bg-white p-4">
+                <QRCodeSVG
+                  value={qrData.value}
+                  size={qrData.size / 8}
+                  bgColor={qrData.bgColor}
+                  fgColor={qrData.fgColor}
+                  level={qrData.level}
+                  includeMargin={false}
+                  // @ts-ignore
+                  imageSettings={
+                    showLogo && {
+                      ...qrData.imageSettings,
+                      height: qrData.imageSettings
+                        ? qrData.imageSettings.height / 8
+                        : 0,
+                      width: qrData.imageSettings
+                        ? qrData.imageSettings.width / 8
+                        : 0,
+                    }
+                  }
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <div className="grid grid-cols-2 gap-2 px-4 sm:px-16">
+                <Button
+                  onClick={() => navigator.clipboard.writeText(props.url)}
+                  className="text-sm"
+                >
+                  <Clipboard className="mr-2 h-4 w-4" /> Copy
+                </Button>
+                <Button
+                  onClick={async () => {
+                    const canvas = await getQRAsCanvas(qrData, "image/png")
+
+                    if (canvas instanceof HTMLCanvasElement) {
+                      const canvasDataURL = canvas.toDataURL("image/png")
+                      download(canvasDataURL, "png")
+                    } else {
+                      download(canvas, "png")
+                    }
+                  }}
+                  className="text-sm "
+                >
+                  <Download className="mr-2 h-4 w-4" /> Download
+                </Button>
+              </div>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      )}
+    </div>
   )
 }
