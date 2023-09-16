480784039ff6cd9044b156ece0fc6f9f10d0afd5
sajjad <samaralishaikh212@gmail.com>
2023-09-16 21:23:26 +0530

    update: search filter

--- src/components/payments/data-table-toolbar.tsx
+++ src/components/payments/data-table-toolbar.tsx
@@ -44,10 +44,12 @@ export function DataTableToolbar<TData>({
     <div className="flex items-center justify-between">
       <div className="flex flex-1 items-center space-x-2">
         <Input
-          placeholder="Filter invoice..."
-          value={(table.getColumn("id")?.getFilterValue() as string) ?? ""}
+          placeholder="Seach Customer..."
+          value={
+            (table.getColumn("customerName")?.getFilterValue() as string) ?? ""
+          }
           onChange={(event) =>
-            table.getColumn("id")?.setFilterValue(event.target.value)
+            table.getColumn("customerName")?.setFilterValue(event.target.value)
           }
           className="h-8 w-[150px] lg:w-[250px]"
         />
