f3fa2c2931f4b47cabc5b23667f7c70125e71bbf
sajjad <samaralishaikh212@gmail.com>
2023-10-11 17:02:42 +0530

    add redux RTK query example

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,19 @@
 import counterReducer from "@/store/features/counter-slice"
+import { userApi } from "@/store/services/user-api"
 import { configureStore } from "@reduxjs/toolkit"
+import { setupListeners } from "@reduxjs/toolkit/dist/query"
 
 export const store = configureStore({
   reducer: {
     counterReducer,
+    [userApi.reducerPath]: userApi.reducer,
   },
   devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({}).concat([userApi.middleware]),
 })
+
+setupListeners(store.dispatch)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
