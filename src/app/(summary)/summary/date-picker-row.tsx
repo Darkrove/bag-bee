ec5545d46817bbc5bb54a40221915eb6b7e14b90
Shaikh <ext-sajjadabdul.shaikh@here.com>
2023-12-19 17:07:43 +0530

    change in row comp

--- src/app/(summary)/summary/date-picker-row.tsx
+++ src/app/(summary)/summary/date-picker-row.tsx
@@ -5,8 +5,7 @@ import DatePicker from "@/components/datepicker"
 
 export const Row = () => {
   return (
-    <div className="grid grid-cols-3">
-      <h2 className="text-3xl font-bold tracking-tight col-span-2">Summary</h2>
+    <div className="flex justify-end">
       <DatePicker />
     </div>
   )
